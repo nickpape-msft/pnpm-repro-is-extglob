81961eb96bd67b10e06e3017669d8ad30e827d27
Nick Pape nickpape <nickpape@microsoft.com>
2018-08-07 12:51:37 -0700

    Attempting to create a minimalist repro, but it didn't work.

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const gulp = require('gulp');
+const rimraf = require('rimraf');
+const wrapAMD = require('gulp-wrap-amd');
+
+gulp.task('default', () => {
+  console.log('Success');
+});
